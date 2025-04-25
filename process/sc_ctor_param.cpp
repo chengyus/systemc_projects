7cf9881a7791dcb2380d3cecad37d9bc94de1372
Joseph Sung <chengyu.sung@gmail.com>
2025-04-25 14:42:29 +0800

    sc_has_process deprecated, using sc_ctor with param

--- process/sc_ctor_param.cpp
+++ process/sc_ctor_param.cpp
@@ -17,8 +17,9 @@ SC_MODULE(MODULE_B1) { // constructor with module name as the only input argumen
   }
 };
 
+//2025/04/24 NOTE: SystemC 3.02 compile gave deprecation warning about SC_HAS_PROCESS.
 SC_MODULE(MODULE_B2) { // constructor with module name as the only input argument
-  SC_HAS_PROCESS(MODULE_B2); // no implicit constructor declarition
+  //SC_HAS_PROCESS(MODULE_B2); // no implicit constructor declarition
   MODULE_B2(sc_module_name name) { // explicit constructor declaration, also instantiate base class by default via sc_module(name)
     SC_METHOD(func_b); // register member function
   }
@@ -27,14 +28,20 @@ SC_MODULE(MODULE_B2) { // constructor with module name as the only input argumen
   }
 };
 
+//changing it with IEEE 1666-2023 update slids on p18
 SC_MODULE(MODULE_C) { // pass additional input argument(s)
-  const int i;
-  SC_HAS_PROCESS(MODULE_C); // OK to use SC_CTOR, which will also define an un-used constructor: MODULE_A(sc_module_name);
-  MODULE_C(sc_module_name name, int i) : i(i) { // define the constructor function
-    SC_METHOD(func_c); // register member function
+  int i;
+  //SC_HAS_PROCESS(MODULE_C); // OK to use SC_CTOR, which will also define an un-used constructor: MODULE_A(sc_module_name);
+  //MODULE_C(sc_module_name name, int i) : i(i) { // define the constructor function
+  //  SC_METHOD(func_c); // register member function
+  //}
+  SC_CTOR(MODULE_C, int i) : i(i)
+  {
+    SC_METHOD(func_c);
   }
+
   void func_c() { // define function
-    std::cout << name() << ", additional input argument" << std::endl;
+    std::cout << name() << ", additional input argument i: " << i << std::endl;
   }
 };
 
@@ -48,8 +55,9 @@ MODULE_D1::MODULE_D1(sc_module_name name) : sc_module(name) { // defines constru
   SC_METHOD(func_d);
 }
 
+//2025/04/24 NOTE: SystemC 3.02 compile gave deprecation warning about SC_HAS_PROCESS.
 SC_MODULE(MODULE_D2) { // SC_HAS_PROCESS inside header, constructor defined outside header
-  SC_HAS_PROCESS(MODULE_D2);
+  //SC_HAS_PROCESS(MODULE_D2);
   MODULE_D2(sc_module_name); // declares constructor
   void func_d() {
     std::cout << this->name() << ", SC_CTOR inside header, constructor defined outside header" << std::endl;
@@ -65,8 +73,9 @@ SC_MODULE(MODULE_E) { // SC_CURRENT_USER_MODULE and constructor defined outside 
     std::cout << this->name() << ", SC_HAS_PROCESS outside header, CANNOT use SC_CTOR" << std::endl;
   }
 };
+//2025/04/24 NOTE: SystemC 3.02 compile gave deprecation warning about SC_HAS_PROCESS.
 MODULE_E::MODULE_E(sc_module_name name) { // constructor definition
-  SC_HAS_PROCESS(MODULE_E); // NOT OK to use SC_CTOR
+  //SC_HAS_PROCESS(MODULE_E); // NOT OK to use SC_CTOR
   SC_METHOD(func_e);
 }
 
@@ -74,7 +83,7 @@ int sc_main(int, char*[]) {
   MODULE_A module_a("module_a");
   MODULE_B1 module_b1("module_b1");
   MODULE_B2 module_b2("module_b2");
-  MODULE_C module_c("module_c", 1);
+  MODULE_C module_c("module_c", 3);
   MODULE_D1 module_d1("module_d1");
   MODULE_D2 module_d2("module_d2");
   MODULE_E module_e("module_e");
